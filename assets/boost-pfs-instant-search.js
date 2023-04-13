d01d242e75b0304d03aa325bd06efa81d5c13247
Amy Mick <amymick847@gmail.com>
2023-04-12 19:03:20 -0500

    first commit - from Wholesale Club Theme 4-10

---
+++ assets/boost-pfs-instant-search.js
@@ -0,0 +1,53 @@
+// Override Settings
+var boostPFSInstantSearchConfig = {
+	search: {
+			
+	}
+};
+
+(function () {  
+	BoostPFS.inject(this);  
+
+	// Customize style of Suggestion box
+	SearchInput.prototype.customizeInstantSearch = function(suggestionElement, searchElement, searchBoxId) {
+			 if (!suggestionElement) suggestionElement = this.$uiMenuElement;   
+		if (!searchElement) searchElement = this.$element;  
+		if (!searchBoxId) searchBoxId = this.id; 
+	};
+
+	BoostPFS.prototype.beforeBuildSearchBox = function (id) {
+
+		// Remove theme's instant search events
+		var cloneSearchBar = jQ(id).clone();
+		jQ(id).replaceWith(cloneSearchBar);      
+		
+		
+		if (Utils.InstantSearch.isFullWidthMobile()) {
+			jQ(id).removeAttr('autofocus');
+			if (jQ(id).is(':focus')) {
+				jQ(id).blur();
+			}
+		}
+	};
+
+	BoostPFS.prototype.afterCloseSuggestionMobile = function(searchBoxId, isCloseSearchBox) {
+		// Close theme's search pop up
+		if (isCloseSearchBox && window.$ && $.fancybox){
+			$.fancybox.close(true);
+		}
+	};
+  
+    var bindEvents = InstantSearchMobile.prototype.bindEvents;
+  
+    InstantSearchMobile.prototype.bindEvents = function() {
+        bindEvents.call(this);
+
+        var self = this;
+
+        jQ('[data-show-search-trigger]').click(function(e) {
+          e.stopPropagation();
+          e.preventDefault();
+          self.openSuggestionMobile()
+        });
+    }
+})();
---
+++ assets/swatchify-data.js
@@ -0,0 +1,8 @@
+
+(function() {
+  if(!window['SellerPanda'] || !window['SellerPanda']['Swatchify']){
+    console.error('Swatchify error! Please contact support@sellerpanda.com')
+  } else {
+    window['SellerPanda']['Swatchify'].swatchify_SetSwatches({swatches: {"Size":{"Gold":"c#FFD700"},"Color":{"Red":"c#FF0000","Tan":"c#D2B48C","Aqua":"c#00FFFF","Blue":"c#0000FF","Gold":"c#FFD700","Gray":"c#808080","Grey":"c#808080","NAVY":"c#000080","Navy":"c#000080","Pink":"c#FFC0CB","Plum":"c#DDA0DD","Teal":"c#008080","Azure":"c#F0FFFF","BLACK":"c#000000","Beige":"c#F5F5DC","Black":"c#000000","Brown":"c#A52A2A","Coral":"c#FF7F50","Green":"c#008000","Ivory":"c#FFFFF0","Ocean":"c#CCF2F1","Olive":"c#808000","White":"c#FFFFFF","Purple":"c#800080","Silver":"c#C0C0C0","Violet":"c#EE82EE","Magenta":"c#FF00FF","Lavender":"c#E6E6FA","Amazonite":"c#286E5C","Chocolate":"c#D2691E","Turquoise":"c#40E0D0","Anthracite":"c#AEAEAE"}}, specific: {}});
+  }
+}())
